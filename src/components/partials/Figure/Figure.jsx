b0e0b9d666455ed5bbb886dbd1669b5dd84c4e78
GuillermoCochrane <guilleac81@gmail.com>
2024-07-22 19:48:25 -0300

    Funcionalidad de zoom para imágenes en vista mobile

--- src/components/partials/Figure/Figure.jsx
+++ src/components/partials/Figure/Figure.jsx
@@ -1,12 +1,23 @@
 import PropTypes from "prop-types";
 const apiUrl = import.meta.env.VITE_API_URL;
+import { useRef } from "react";
 
 function Figure({Data}) {
+  const $img = useRef(null);
+
+	const zoom = () => {
+		if (window.innerWidth < 768) {
+			$img.current.classList.toggle('enlarged');
+	}}
+
   return (
     <figure>
         <img 
           src={`${apiUrl}${Data.img}`}
           alt={Data.alt}
+          title={Data.alt}
+          onClick={zoom}
+          ref={$img}
           className={ 
             Data.style == "icon" ? "icon" : 
             Data.style == "info" ? "info" : 
