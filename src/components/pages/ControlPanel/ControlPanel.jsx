e6d379df562b1e4f989d6c737ac4a0d6c9367d62
GuillermoCochrane <guilleac81@gmail.com>
2024-08-05 20:22:07 -0300

    Componente base del panel de control

---
+++ src/components/pages/ControlPanel/ControlPanel.jsx
@@ -0,0 +1,49 @@
+import  { useRef } from 'react';
+import { Route, Switch, Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import Sections from '../Sections/Sections';
+import Error404 from "../Error404/Error404.jsx";
+
+function ControlPanel({ match }) {
+    const navBar = useRef(null);
+    let title = "";
+    const handleClick = () => {
+    navBar.current.classList.toggle("hidden");
+    
+    }
+    return (
+        <>
+            <header>
+                <button onClick={handleClick} id="index">Menú</button>
+                <h1>  {`Panel de Control ${title} `}</h1>
+            </header>
+            <main>
+                <nav className='sumario' ref={navBar}>
+                    <ol>
+                        <li>
+                            <Link to="/controlPanel/newClass">Nueva Clase</Link>
+                        </li>
+                        <li>
+                            <Link to="/controlPanel/newTopic">Nuevo Tema</Link>
+                        </li>
+                        <li>
+                            <Link to="/controlPanel/newSection">Nueva Sección</Link>
+                        </li>
+                    </ol>
+                </nav>
+                <Switch>
+                    <Route path={`${match.url}`}  component={Sections} />
+                    <Route path={`${match.url}/newClass`}  component={Sections} />
+                    <Route path={`${match.url}/newTopic`}  component={Sections} />
+                    <Route path={`${match.url}/newSection`}  component={Sections} />
+                    <Route component={Error404} />
+                </Switch>
+            </main>
+        </>
+    )
+}
+
+ControlPanel.propTypes = {
+    match: PropTypes.object.isRequired,
+};
+export default ControlPanel
---
+++ src/components/pages/Sections/Sections.jsx
@@ -0,0 +1,9 @@
+function Sections() {
+    return (
+        <article>
+            <h2>Sections</h2>
+        </article>
+    )
+}
+
+export default Sections
