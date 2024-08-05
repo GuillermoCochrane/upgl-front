e6a64f2f5f9c5b46dcd176271eb46bbfb280c821
GuillermoCochrane <guilleac81@gmail.com>
2024-08-05 20:21:23 -0300

    Agregada ruta para panel de control

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Route , Switch } from "react-router-dom";
 import Error404 from "./pages/Error404/Error404"
 import Courses from "./pages/Courses/Courses";
 import Home from "./pages/Home/Home";
+import ControlPanel from "./pages/ControlPanel/ControlPanel";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/courses" exact component={Home} />
+        <Route path="/controlPanel"  component={ControlPanel} />
         <Route path="/courses/:courseId"  component={Courses} />
         <Route component={Error404} />
       </Switch>
