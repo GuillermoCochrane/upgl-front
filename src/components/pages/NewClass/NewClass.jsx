54b95a106d2e271a2a0be036d586df5994aac65a
GuillermoCochrane <guilleac81@gmail.com>
2024-08-07 17:46:58 -0300

    Modifficación para mostrar errores y persistir información, cuando no se pasan las validaciones del backend

--- src/components/pages/NewClass/NewClass.jsx
+++ src/components/pages/NewClass/NewClass.jsx
@@ -6,19 +6,20 @@ function NewClass() {
   let form = useRef(null);
 
   let [validations, setValidations] = useState({});
+  let [oldData, setOldData] = useState( { title: "", summary: "", option: ""});
 
   let createClass = async (e) => {  
     e.preventDefault();
     let courseSelect = form.current.elements.courseSelect.value;
-    let className = form.current.elements.className.value;
+    let title = form.current.elements.title.value;
     let summary = form.current.elements.summary.value;
     let endpoint = `${apiUrl}api/${courseSelect}/newClass`;
     let data = {
       course: courseSelect,
-      class: className,
+      title: title,
       summary: summary,
     };
-    // console.log(data);
+
     let formData = {
       method: 'POST',
       headers: {
@@ -30,11 +31,13 @@ function NewClass() {
     try {
       const response = await fetch(endpoint, formData);
       const data = await response.json();
-      console.log(data.meta.status);
-      if (data.meta.status === 200) {
-        setValidations({success: 'La Clase se creo correctamente'});
+      if (data.meta.created) {
+        setValidations({success: `Se creo una nueva clase en el curso de ${courseSelect}`});
+        setOldData({title: "", summary: "", option: ""});
       } else {
+        data.oldData.option = courseSelect
         setValidations(data.errors);
+        setOldData(data.oldData);
       }
     }
     catch (error) {
@@ -43,38 +46,77 @@ function NewClass() {
     }
   }
 
+  let updateForm = (input, value) => {
+    input == 'title' ? setOldData({...oldData, title: value}) : null;
+    input == 'summary' ? setOldData({...oldData, summary: value}) : null;
+  }
+
   console.log(validations);
 
   return (
     <article>
+
       <form ref={form} onSubmit={createClass}>
 
-        <section>
+        <section className='section-flex'>
           <label htmlFor="courseSelect">Seleccione un Curso</label>
           <select name="courseSelect" id="courseSelect">
-            <option value="ia">IA</option>
-            <option value="python">Python</option>
-            <option value="test">Test</option>
+            <option value="">---</option>
+            <option value="python" selected = {oldData.option == 'python'}> Python </option>
+            <option value="test" selected = {oldData.option == 'test' }> Test </option>
+            <option value="ia"  selected = {oldData.option == 'ia' }> IA </option>
           </select>
-          {validations.course && <span className='error'>{validations.course}</span>}
+          {
+            validations.course && 
+            <span className='error'>
+              {validations.course.msg}
+            </span>
+          }
         </section>
 
-        <section>
+        <section className='section-flex'>
           <label htmlFor="className">Nombre de la Clase</label>
-          <input type="text" name="title" id="className" />
-          {validations.title && <span className='error'>{validations.title}</span>}
+          <input 
+                type="text" 
+                name="title" 
+                id="className" 
+                value={oldData.title} 
+                onChange={(e) => updateForm('title', e.target.value)}
+          />
+          {
+            validations.title && 
+            <span className='error'>
+              {validations.title.msg}
+            </span>
+          }
         </section>
 
-        <section>
+        <section className='section-flex'>
           <label htmlFor="index-title">Titulo para el Indice</label>
-          <input type="text" name="summary" id="index-title" />
-          {validations.summary && <span className='error'>{validations.summary}</span>}
+          <input 
+                type="text" 
+                name="summary" 
+                id="index-title" 
+                value={oldData.summary} 
+                onChange={(e) => updateForm('summary', e.target.value)}
+          />
+          {
+            validations.summary && 
+            <span className='error'>
+              {validations.summary.msg}
+            </span>
+          }
         </section>
 
         <button type="submit">Crear</button>
-        {validations.success && <span className='success'>{validations.success}</span>}
+        {
+          validations.success && 
+          <span className='success'>
+            {validations.success}
+          </span>
+        }
       </form>
-      
+
     </article>
   );
 }
