4ae3ed25348218eeff47efbd58e2f7a675881154
GuillermoCochrane <guilleac81@gmail.com>
2024-08-24 19:49:33 -0300

    Componente para formularios que contiene label, textarea y span de errores

---
+++ src/components/partials/ControlPanel/TextAreaSection/TextAreaSection.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+
+const TextAreaSection = ({ 
+  name,
+  id,
+  label,
+  value,
+  onChange,
+  onBlur,
+  onInput,
+  validations,
+  placeholder, 
+  disabled,
+  styles, 
+  rows,
+  columns
+}) => {
+
+  return (
+    <section className={styles}>
+      <label htmlFor={id}> {label} </label>
+      <textarea 
+        name={name}
+        id={id}
+        value={value}
+        onChange={(e) => onChange(name,e.target.value)}
+        onBlur={onBlur}
+        onInput={onInput}
+        placeholder={placeholder}
+        disabled={disabled}
+        rows={rows}
+        cols={columns}
+      />
+      {
+        <span className='error'>
+          {validations[name] ? validations[name].msg : "\u00A0"}
+        </span> 
+      }
+    </section>);
+};
+
+TextAreaSection.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  validations: PropTypes.object.isRequired,
+  styles: PropTypes.string,
+  onBlur: PropTypes.func,
+  onInput: PropTypes.func,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  rows: PropTypes.number,
+  columns: PropTypes.number,
+};
+
+export default TextAreaSection;
