5255e6c55fd6328fa6ea33ef4cbe6ca5aa376da4
GuillermoCochrane <guilleac81@gmail.com>
2024-08-23 22:14:36 -0300

    Componente para formularios que contiene label, input y span de errores

---
+++ src/components/partials/ControlPanel/InputSection/InputSection.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+
+const InputSection = ({ 
+  type,
+  name,
+  id,
+  label,
+  value,
+  onChange,
+  onBlur,
+  onInput,
+  validations,
+  placeholder, 
+  disabled,
+  styles
+}) => {
+
+  return (
+  <section className={styles}>
+    <label htmlFor={id}> {label} </label>
+    <input
+      type={type}
+      name={name}
+      id={id}
+      value={value}
+      onChange={(e) => onChange(name,e.target.value)}
+      onBlur={onBlur}
+      onInput={onInput}
+      placeholder={placeholder}
+      disabled={disabled}
+    />
+    {
+      <span className='error'>
+        {validations[name] ? validations[name].msg : "\u00A0"}
+      </span> 
+    }
+  </section>);
+};
+
+InputSection.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  styles: PropTypes.string,
+  validations: PropTypes.object,
+  onBlur: PropTypes.func,
+  onInput: PropTypes.func,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+export default InputSection;
