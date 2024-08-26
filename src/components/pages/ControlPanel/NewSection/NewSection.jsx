615b85847f9e737a87de1c84bac40bf4845769aa
GuillermoCochrane <guilleac81@gmail.com>
2024-08-26 02:38:10 -0300

    Agregado selector de tema

--- src/components/pages/ControlPanel/NewSection/NewSection.jsx
+++ src/components/pages/ControlPanel/NewSection/NewSection.jsx
@@ -8,7 +8,8 @@ import Input from "../../../partials/ControlPanel/InputSection/InputSection"
 function NewSection() {
     let [courseSelectors, setCourseSelectors] = useState([]);
     let [classSelectors, setClassSelectors] = useState([]);
-    let [oldData, setOldData] = useState( { classSelect: "", courseSelect: ""});
+    let [topicSelectors, setTopicSelectors] = useState([]);
+    let [oldData, setOldData] = useState( { classSelect: "", courseSelect: "", topicSelect: ""});
 
     const updateForm = (field, value) => {
       setOldData(formValidations.updateInput(field, value, oldData));
@@ -16,8 +17,6 @@ function NewSection() {
 
     useEffect(() => {
       const endpoint = `${apiUrl}api/course/index`;
-      console.log(endpoint);
-      
       const fetchCourses = async () => {
         try {
           const response = await fetch(endpoint);
@@ -50,6 +49,26 @@ function NewSection() {
       }
     }, [oldData.courseSelect]);
     
+    useEffect(() => {
+      if (oldData.courseSelect && oldData.classSelect) {
+        const endpoint = `${apiUrl}api/course/${oldData.courseSelect.toLowerCase()}/classindex/${oldData.classSelect.toLowerCase()}`;
+        const fetchTopics = async () => {
+          try {
+            const response = await fetch(endpoint);
+            const data = await response.json();
+            setTopicSelectors(data.data[0].links);
+          }
+          catch (error) {
+            console.log(error);
+            setTopicSelectors([]);
+          }
+        }
+        fetchTopics();
+      }
+    }, [oldData.courseSelect, oldData.classSelect]);
+
+    console.log(topicSelectors);
+
     return (
         <article>
             <h2>Nueva Sección</h2>
@@ -64,6 +83,7 @@ function NewSection() {
                         label="Seleccione un Curso"
                         onChange={updateForm}
                         options={courseSelectors}
+                        optionReferences={{value: "id", name: "name"}}
                 />
                 
                 <Select
@@ -76,6 +96,20 @@ function NewSection() {
                         options={classSelectors}
                         selectStyles={"error"}
                         optionMsg={"Seleccione un Curso para ver las Clases disponibles"}
+                        optionReferences={{value: "classID", name: "summary"}}
+                />
+
+                <Select
+                        styles={"section-flex"}
+                        name="topicSelect"
+                        id="topicSelect"
+                        value={oldData.topicSelect}
+                        label="Seleccione un Tema"
+                        onChange={updateForm}
+                        options={topicSelectors}
+                        selectStyles={"error"}
+                        optionMsg={"Seleccione una Clase para ver los Temas disponibles"}
+                        optionReferences={{value: "topicID", name: "title"}}
                 />
 
             </form>
