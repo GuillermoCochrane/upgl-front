f37295268294c93cd136466abe26c3bf4bfd627c
GuillermoCochrane <guilleac81@gmail.com>
2024-09-01 19:20:28 -0300

    Eliminados métodos en desuso

--- src/utilities/formValidations.js
+++ src/utilities/formValidations.js
@@ -3,22 +3,6 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 const formValidations = {
 
-    updateInput: (input, value, oldData) => {
-        let newData = {...oldData};
-        newData[input] = value;
-        return newData;
-    },
-
-    validationsAlerts (input, validations, form) {
-        if (validations[input]) {
-            form.current.elements[input].classList.remove('success');
-            form.current.elements[input].classList.add('error');
-        } else {
-            form.current.elements[input].classList.remove('error');
-            form.current.elements[input].classList.add('success');
-        }
-    },
-    
     required: (input, error, form, oldValidations ) => {
         let inputField = form.current.elements[input].value;
         let newValidations = { ...oldValidations };
