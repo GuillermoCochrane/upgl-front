aee03bc2d45d9426d67a4f43017e8cb5b521432f
GuillermoCochrane <guilleac81@gmail.com>
2024-09-01 18:40:11 -0300

    Componente para crear enlaces

--- src/components/pages/ControlPanel/NewSection/NewLink/NewLink.jsx
+++ src/components/pages/ControlPanel/NewSection/NewLink/NewLink.jsx
@@ -1,14 +1,80 @@
 import { useState, useEffect, useRef } from "react";
 import PropTypes from 'prop-types';
-const apiUrl = import.meta.env.VITE_API_URL;
+import Input from "../../shared/InputSection/InputSection";
+import Select from "../../shared/SelectSection/SelectSection";
 import formValidations from "../../../../../utilities/formValidations";
+import utilities from "../../../../../utilities/utilities";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 
-const NewLink = ({ courseID, classID, topicID  }) => {
+const NewLink = ({ courseID, classID, topicID, reset  }) => {
+  let [validations, setValidations] = useState({});
+  let [stylesSelectors, setStylesSelectors] = useState([]);
+  let [oldData, setOldData] = useState({ text: "", content: "", link: "" });
+  const form = useRef(null);
+
+  const updateForm = (field, value) => {
+    setOldData(formValidations.updateInput(field, value, oldData));
+  };
+
+  useEffect(() => {
+    const endpoint = `${apiUrl}api/controlpanel/styles`;
+      const fetchCourses = async () => {
+        try {
+          const response = await fetch(endpoint);
+          const data = await response.json();
+          setStylesSelectors(data.data);
+        }
+        catch (error) {
+          console.log(error);
+          setStylesSelectors([]);
+        }
+      }
+      fetchCourses();
+  }, []);
+
   return (
-    <form className="panel-form">
-      <label htmlFor="">Hipervínculo</label>
-      <input type="text" />
+    <form className="panel-form" ref={form}>
+
+      <Input
+        type = "text"
+        name = "text"
+        id = "text"
+        value = {oldData.text}
+        label = "Texto del enlace"
+        onChange = {updateForm}
+        styles = {"section-flex"}
+        validations = {validations}
+      />
+
+      <Select
+        styles = {"section-flex"}
+        name = "content"
+        id = "content"
+        value = {oldData.content}
+        label = "Estilo del texto"
+        onChange = {updateForm}
+        options = {stylesSelectors}
+        validations = {validations}
+        optionReferences ={{value: "id", name: "title"}}
+      />
+
+      <Input
+        type = "text"
+        name = "link"
+        id = "link"
+        value = {oldData.link}
+        label = "URL del enlace"
+        onChange = {updateForm}
+        styles = {"section-flex"}
+        validations = {validations}
+        />
+
+        <span className="success">
+          {validations.success ? validations.success : "\u00A0 "}
+        </span>
+
+      <button type="submit">Crear</button>
     </form>
   )
 }
@@ -17,6 +83,7 @@ NewLink.propTypes = {
   courseID: PropTypes.string.isRequired,
   classID: PropTypes.string.isRequired,
   topicID: PropTypes.string.isRequired,
+  reset: PropTypes.func.isRequired,
 };
 
 export default NewLink;
