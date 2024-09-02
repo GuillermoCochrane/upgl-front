887253d88a4bc0b5321c0bc621e73fc66d58abb8
GuillermoCochrane <guilleac81@gmail.com>
2024-09-02 02:10:33 -0300

    Componente para la creación de enlaces de descargas externas

--- src/components/pages/ControlPanel/NewSection/NewDownload/NewDownload.jsx
+++ src/components/pages/ControlPanel/NewSection/NewDownload/NewDownload.jsx
@@ -1,14 +1,72 @@
 import { useState, useEffect, useRef } from "react";
 import PropTypes from 'prop-types';
-const apiUrl = import.meta.env.VITE_API_URL;
+import Input from "../../shared/InputSection/InputSection";
+import Select from "../../shared/SelectSection/SelectSection";
 import formValidations from "../../../../../utilities/formValidations";
+import utilities from "../../../../../utilities/utilities";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 
-const NewDownload = ({ courseID, classID, topicID  }) => {
+const NewDownload = ({ courseID, classID, topicID, reset  }) => {
+  let [validations, setValidations] = useState({});
+  let [stylesSelectors, setStylesSelectors] = useState([]);
+  let [oldData, setOldData] = useState({ text: "", content: "", link: "" });
+  const form = useRef(null);
+
+  const updateForm = (field, value) => {
+    setOldData(utilities.updateInput(field, value, oldData));
+  };
+
+  useEffect(() => {
+    let fetchStyles = async () => {
+      let stylesData = await utilities.fetchStylesData();
+      setStylesSelectors(stylesData);
+    }
+      fetchStyles();
+  }, []);
+
   return (
-    <form className="panel-form">
-      <label htmlFor="">Descargas</label>
-      <input type="text" />
+    <form className="panel-form" ref={form} >
+
+      <Input
+        type = "text"
+        name = "text"
+        id = "text"
+        value = {oldData.text}
+        label = "Texto de la descarga"
+        onChange = {updateForm}
+        styles = {"section-flex"}
+        validations = {validations}
+      />
+
+      <Select
+        styles = {"section-flex"}
+        name = "content"
+        id = "content"
+        value = {oldData.content}
+        label = "Estilo del texto"
+        onChange = {updateForm}
+        options = {stylesSelectors}
+        validations = {validations}
+        optionReferences ={{value: "id", name: "title"}}
+      />
+
+      <Input
+        type = "text"
+        name = "link"
+        id = "link"
+        value = {oldData.link}
+        label = "URL de la descarga"
+        onChange = {updateForm}
+        styles = {"section-flex"}
+        validations = {validations}
+        />
+
+        <span className="success">
+          {validations.success ? validations.success : "\u00A0 "}
+        </span>
+
+      <button type="submit">Crear</button>
     </form>
   )
 }
@@ -17,6 +75,7 @@ NewDownload.propTypes = {
   courseID: PropTypes.string.isRequired,
   classID: PropTypes.string.isRequired,
   topicID: PropTypes.string.isRequired,
+  reset: PropTypes.func.isRequired,
 };
 
 export default NewDownload;
