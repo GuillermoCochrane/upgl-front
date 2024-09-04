99edc3308def2de9b574b40f40a328bf09765335
GuillermoCochrane <guilleac81@gmail.com>
2024-09-04 03:39:35 -0300

    Implementadas validaciones de frontend para todos los campos del formulario

--- src/components/pages/ControlPanel/NewSection/NewYoutube/NewYoutube.jsx
+++ src/components/pages/ControlPanel/NewSection/NewYoutube/NewYoutube.jsx
@@ -9,12 +9,64 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 const NewYoutube = ({ courseID, classID, topicID, reset  }) => {
   let [validations, setValidations] = useState({});
-  let [stylesSelectors, setStylesSelectors] = useState([]);
   let [oldData, setOldData] = useState({ title: "",  link: "", width: "", height: "" });
   const form = useRef(null);
 
+  const titleError = "El titulo del video";
+  const linkError = "La URL";
+  const widthError = "El ancho del video";
+  const heightError = "El alto del video";
+
   const updateForm = (field, value) => {
     setOldData(utilities.updateInput(field, value, oldData));
+  };
+
+  const validateTitle = () => {
+    let newValidations = formValidations.required('title', titleError, form, validations);
+    if (form.current.elements['title'].value) {
+        newValidations = formValidations.min('title', titleError, form, newValidations, 3); 
+        if (!newValidations.title) {
+          newValidations = formValidations.max('title', titleError, form, newValidations, 35);
+        }
+    } 
+    setValidations(newValidations);
+    utilities.validationsAlerts('title', newValidations, form);
+  };
+
+  const validateLink = () => {
+    let newValidations = formValidations.required('link', linkError, form, validations);
+    if (form.current.elements['link'].value) {
+        newValidations = formValidations.min('link', linkError, form, newValidations, 9);
+        if (!newValidations.link) {
+            newValidations = formValidations.url('link', linkError, form, newValidations);
+        }
+    }
+    setValidations(newValidations);
+    utilities.validationsAlerts('link', newValidations, form);
+  };
+
+  const validatHeight = () => {
+    let newValidations = formValidations.required('height', heightError, form, validations);
+    if (form.current.elements['height'].value) {
+        newValidations = formValidations.numeric('height', heightError, form, newValidations);
+        if (!newValidations.height) {
+            newValidations = formValidations.min('height', heightError, form, newValidations, 2);
+        }
+    }
+    setValidations(newValidations);
+    utilities.validationsAlerts('height', newValidations, form);
+  };
+
+  const validatWidth = () => {
+    let newValidations = formValidations.required('width', widthError, form, validations);
+    if (form.current.elements['width'].value) {
+        newValidations = formValidations.numeric('width', widthError, form, newValidations);
+        if (!newValidations.width) {
+            newValidations = formValidations.min('width', widthError, form, newValidations, 2);
+        }
+    }
+    setValidations(newValidations);
+    utilities.validationsAlerts('width', newValidations, form);
   };
 
   const createYouTube = async (e) => {
@@ -56,6 +108,8 @@ const NewYoutube = ({ courseID, classID, topicID, reset  }) => {
         value = {oldData.title}
         label = "Titulo del video"
         onChange = {updateForm}
+        onBlur={validateTitle}
+        onInput={validateTitle}
         styles = {"section-flex"}
         validations = {validations}
       />
@@ -67,6 +121,8 @@ const NewYoutube = ({ courseID, classID, topicID, reset  }) => {
         value = {oldData.link}
         label = "URL del video"
         onChange = {updateForm}
+        onBlur={validateLink}
+        onInput={validateLink}
         styles = {"section-flex"}
         validations = {validations}
         />
@@ -78,6 +134,8 @@ const NewYoutube = ({ courseID, classID, topicID, reset  }) => {
           value = {oldData.width}
           label = "Ancho del video"
           onChange = {updateForm}
+          onBlur = {validatWidth}
+          onInput = {validatWidth}
           styles = {"section-flex"}
           validations = {validations}
           />
@@ -89,6 +147,8 @@ const NewYoutube = ({ courseID, classID, topicID, reset  }) => {
             value = {oldData.height}
             label = "Alto del video"
             onChange = {updateForm}
+            onBlur = {validatHeight}
+            onInput = {validatHeight}
             styles = {"section-flex"}
             validations = {validations}
             />
