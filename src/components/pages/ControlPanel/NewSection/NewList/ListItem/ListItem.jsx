b6947d16756af330b5b5ee4f78577724e7318914
GuillermoCochrane <guilleac81@gmail.com>
2024-09-17 18:01:14 -0300

    Nuevo componente que maneja los campos del formulario de cada item de lista /stub

---
+++ src/components/pages/ControlPanel/NewSection/NewList/ListItem/ListItem.jsx
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import Input from "../../../shared/InputSection/InputSection";
+import Select from "../../../shared/SelectSection/SelectSection";
+
+function ListItem( {item, index, validations, stylesSelectors, updateItem,  validateText, validateContent, validationsManager, removeItem}) {
+    return (
+        <article className="list-item">
+            <aside>
+
+                <Input
+                    type="text"
+                    name={`item-${item.id}-text`}
+                    id={`${item.id}-text`}
+                    value={item.text}
+                    onChange={updateItem}
+                    onBlur={() => validateText(item.id)}
+                    onInput={() => validateText(item.id)}
+                    label={`Item de lista: ${index + 1}`}
+                    styles={"section-flex"}
+                    itemID={item.id}
+                    stateField={"text"}
+                    validations={validationsManager(item.id, validations)}
+                />
+
+                <Select
+                    styles={"section-flex"}
+                    name={`item-${item.id}-content`}
+                    id={`${item.id}-content`}
+                    value={item.content}
+                    label="Estilo del texto"
+                    onChange={updateItem}
+                    onBlur={() => validateContent(item.content, item.id)}
+                    options={stylesSelectors}
+                    validations={validationsManager(item.id, validations)}
+                    optionReferences={{value: "id", name: "title"}}
+                    itemID={item.id}
+                    stateField={"content"}
+                />
+            </aside>
+
+            <button type="button" onClick={() => removeItem(item.id)}>Eliminar</button>
+
+        </article>
+    )   
+}
+
+ListItem.propTypes = {
+    item: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    validations: PropTypes.array.isRequired,
+    stylesSelectors: PropTypes.array.isRequired,
+    updateItem: PropTypes.func.isRequired,
+    validateText: PropTypes.func.isRequired,
+    validateContent: PropTypes.func.isRequired,
+    validationsManager: PropTypes.func.isRequired,
+    removeItem: PropTypes.func.isRequired,
+};
+
+export default ListItem;
