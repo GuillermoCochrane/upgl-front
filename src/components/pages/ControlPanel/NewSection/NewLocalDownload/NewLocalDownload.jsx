d314590b1e77280a2997f20dbdaf25d8068fff40
GuillermoCochrane <guilleac81@gmail.com>
2024-09-30 17:53:36 -0300

    Implementadas validaciones de frontend para NewLocalFile

--- src/components/pages/ControlPanel/NewSection/NewLocalDownload/NewLocalDownload.jsx
+++ src/components/pages/ControlPanel/NewSection/NewLocalDownload/NewLocalDownload.jsx
@@ -6,6 +6,10 @@ import utilities from "../../../../../utilities/utilities";
 import formValidations from "../../../../../utilities/formValidations";
 const apiUrl = import.meta.env.VITE_API_URL;
 
+const textError = "El texto del enlace";
+const fileError = "El Archivo";
+
+
 const NewLocalDownload = ({ courseID, classID, topicID, reset  }) => {
   const [validations, setValidations] = useState({});
   const [oldData, setOldData] = useState({ text: "", file: null });
@@ -21,6 +25,27 @@ const NewLocalDownload = ({ courseID, classID, topicID, reset  }) => {
     }
   };
 
+  const validateText = () => {
+    let newValidations = formValidations.required('text', textError, form, validations);
+    if (form.current.elements['text'].value) {
+        newValidations = formValidations.min('text', textError, form, newValidations, 2);
+        if (!newValidations.text) {
+            newValidations = formValidations.max('text', textError, form, newValidations, 30);
+        }
+    }
+    setValidations(newValidations);
+    utilities.validationsAlerts('text', newValidations, form);
+  };
+
+  const validateFile = () => {
+    let allowedExtensions = [".zip", ".rar", ".pdf", ".txt", ".md", ".docx", ".xlsx", ".pptx"];
+    let newValidations = formValidations.requiredFile('file', fileError, form, validations);
+    if (form.current.elements['file'].files.length) {
+        newValidations = formValidations.fileExtension('file', form, newValidations, allowedExtensions);
+    }
+    setValidations(newValidations);
+    utilities.validationsAlerts('file', newValidations, form);
+  };
 
   const createDownload = async (e) => {
     e.preventDefault();
@@ -66,6 +91,8 @@ const NewLocalDownload = ({ courseID, classID, topicID, reset  }) => {
         value={oldData.text}
         label="Texto del enlace"
         onChange={updateForm}
+        onBlur={validateText}
+        onInput={validateText}
         styles="section-flex"
         validations={validations}
       />
@@ -75,6 +102,7 @@ const NewLocalDownload = ({ courseID, classID, topicID, reset  }) => {
         id="file"
         label="Archivo para descargar"
         onChange={updateFileForm}
+        onBlur={validateFile}
         styles="section-flex"
         validations={validations}
       />
