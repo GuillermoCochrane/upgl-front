7978618ecd5e17c5f3135d1bd896130f499fe227
GuillermoCochrane <guilleac81@gmail.com>
2024-10-18 18:10:53 -0300

    Modificado componente para procesar diferentes estilos del párrafo

--- src/components/pages/Courses/Topics/P/PTag.jsx
+++ src/components/pages/Courses/Topics/P/PTag.jsx
@@ -1,5 +1,8 @@
 import PropTypes from "prop-types";
 import Bold from "../shared/Styles/Bold/Bold"
+import Italic from "../shared/Styles/Italic/Italic"
+import Underline from "../shared/Styles/Underline/Underline"
+import Mark from "../shared/Styles/Mark/Mark"
 import { Fragment } from "react";
 
 function PTag({Data}) {
@@ -13,6 +16,15 @@ function PTag({Data}) {
                         }
                         {
                             item.content == "bold" && <Bold key = {index} Data = {item.text} />
+                        }
+                        {
+                            item.content == "italic" && <Italic key = {index} Data = {item.text} />
+                        }
+                        {
+                            item.content == "underline" && <Underline key = {index} Data = {item.text} />
+                        }
+                        {
+                            item.content == "mark" && <Mark key = {index} Data = {item.text} />
                         }
                     </Fragment>
                 ))
