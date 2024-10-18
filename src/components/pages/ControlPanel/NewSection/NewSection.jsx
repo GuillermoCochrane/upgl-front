e534f9eb5cbee03b3345dd63eb06d0e0b353f465
GuillermoCochrane <guilleac81@gmail.com>
2024-10-18 19:13:47 -0300

    Modificada ruta de acceso al componente

--- src/components/pages/ControlPanel/NewSection/NewSection.jsx
+++ src/components/pages/ControlPanel/NewSection/NewSection.jsx
@@ -5,7 +5,7 @@ import utilities from "../../../../utilities/utilities";
 import Select from "../shared/SelectSection/SelectSection";
 import NewMainTitle from "./NewMainTitle/NewMainTitle";
 import NewSecondaryTitle from "./NewSecondaryTitle/NewSecondaryTitle";
-import Paragraph from "./NewParagarph/NewParagraph";
+import Paragraph from "./NewParagraph/NewParagraph";
 import NewLink from "./NewLink/NewLink";
 import Download from "./NewDownload/NewDownload";
 import LocalDownload from "./NewLocalDownload/NewLocalDownload";
