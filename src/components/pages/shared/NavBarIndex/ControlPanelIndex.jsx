0081a809698c02e5fa9c31d8f414ec50d786ca95
GuillermoCochrane <guilleac81@gmail.com>
2024-10-22 19:25:11 -0300

    Modificado componente para procesar correctamente la nueva estructura de datos del backend

--- src/components/pages/shared/NavBarIndex/ControlPanelIndex.jsx
+++ src/components/pages/shared/NavBarIndex/ControlPanelIndex.jsx
@@ -1,29 +1,28 @@
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import ControlPanelLi from "./ControlPanelLi";
 
-function NavIndex({linkData}) {
-    const [active, setActive] = useState(false);
-    let urlArray = location.pathname.split("/");
-    let sectionParams = urlArray[2];
-    
-    useEffect(() => {
-        linkData.topicID === sectionParams
-        ? setActive(true) : setActive(false);
-    }, [linkData.topicID, sectionParams]);
-
+function ControlPanelIndex({data}) {
     return (
-        <li className={active ? "current" : ''}>
-            <i className={`fa-solid ${linkData.icon}`}></i>
-            <Link to={linkData.link } className={active ? "current" : ''}>
-                {linkData.title}
-            </Link>
+        <li>
+            <details name="nav">
+                <summary>
+                    <i className={`fa-solid ${data.icon}`}></i>
+                    {data.summary}
+                </summary>
+                <ul>
+                {
+                    data.links.map((link, index) =>
+                        <ControlPanelLi key={index} linkData={link} />
+                    )
+                }
+                </ul>
+            </details>
         </li>
     );
 }
 
-NavIndex.propTypes = {
-    linkData: PropTypes.object.isRequired,
+ControlPanelIndex.propTypes = {
+    data: PropTypes.object.isRequired,
 }
 
-export default NavIndex;
+export default ControlPanelIndex;
