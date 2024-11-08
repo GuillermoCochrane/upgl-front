4add5cc907191d0e103df0daca7457ce66f3e3b5
GuillermoCochrane <guilleac81@gmail.com>
2024-11-08 18:33:57 -0300

    Implementado componente loader, en el componente home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,22 +1,28 @@
 import {Link} from "react-router-dom";
 import Logo from "../../../assets/images/logoUPGL.png";
 import { useEffect, useState } from "react";
+import Loader from "../shared/Loader/Loader";
 const apiUrl = import.meta.env.VITE_API_URL;
+console.log(Logo);
 
 function Home() {
   const [coursesLinks, setCoursesLinks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const endpoint = `${apiUrl}api/course/index`;
+    setLoading(true);
         const fetchData = async () => {
             try {
                 const response = await fetch(endpoint);
                 const data = await response.json();
                 setCoursesLinks(data.data);
+                setLoading(false);
             }
             catch (error) {
                 console.log(error);
                 setCoursesLinks([]);
+                setLoading(false);
             }
         }
         fetchData()
@@ -27,17 +33,21 @@ function Home() {
         <img src={Logo} alt="Logo de UPGL" className="logo" />
         <h1 className="UPGL" >Universidad Popular General Levalle</h1>
         <h2>Seleccione su curso</h2>
-        <section>
         {
-          coursesLinks.map((item, index) => (
-            <h3 key={index}>
-              <Link to={item.link} key={index}>
-                {item.name}
-              </Link>
-            </h3>
-          ))
+          loading ? 
+            <Loader loaderStyles={"home"}  /> :
+            <section>
+              {
+                  coursesLinks.map((item, index) => (
+                    <h3 key={index}>
+                      <Link to={item.link} key={index}>
+                        {item.name}
+                      </Link>
+                    </h3>
+                ))
+              }
+            </section>
         }
-        </section>
     </article>
   )
 }
